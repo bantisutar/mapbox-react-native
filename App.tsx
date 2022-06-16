56fc8c05ff408e1ea32ffec6cf0edf89bf16f81c
bantisutar <coolsutar.banti@gmail.com>
2022-06-16 18:36:44 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import MapContainer from './src/MapViewModule/component/MapContainer';
 const MapboxGL = require('@react-native-mapbox-gl/maps').default;
 
 MapboxGL.setAccessToken(
-  'pk.eyJ1Ijoicm9zaGFucmFtZGFzMTciLCJhIjoiY2thcWZ3dzBrMGJ0dzJ4cHcxeXJoMWZpNSJ9.iTZM4T8POBxb-Eb2mTCAHA',
+  <YOUR_ACCESS_TOKEN>
 );
 MapboxGL.Logger.sharedInstance().stop();
 MapboxGL.setTelemetryEnabled(false);
